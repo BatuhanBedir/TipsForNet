5f6d944e96b932625f77f732e58b0a5ff55b012f
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-17 16:43:46 +0300

    v2

--- SingleOrUnique/Program.cs
+++ SingleOrUnique/Program.cs
@@ -8,12 +8,13 @@ internal class Program
     static void Main(string[] args)
     {
         AppDbContext context = new();
-        Product product = new()
-        {
-            Name = "Product0"
-        };
-        context.Products.Add(product);
-        context.SaveChanges();
+        //Product product = new()
+        //{
+        //    Name = "Product0"
+        //};
+        //context.Products.Add(product);
+        //context.SaveChanges();
+        //SqlException: Cannot insert duplicate key row in object 'dbo.Products' with unique index 'IX_Products_Name'. The duplicate key value is (Product0).
 
         //Product? product = context.Products.Where(x => x.Name == "Product0").SingleOrDefault();
         //System.InvalidOperationException: 'Sequence contains more than one element'
