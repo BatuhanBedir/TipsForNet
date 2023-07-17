da20039d31182cc61a5bcc02011d8debc324df5f
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-17 18:40:46 +0300

    Record

---
+++ Record/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Record.Dtos;
+
+namespace Record.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("[action]")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            loginDto.Email = string.Empty;
+            return Ok("başarılı");
+        }
+
+
+        [HttpPost("[action]")]
+        public IActionResult LoginWithRecord(LoginDtoRecord loginDtoRecord)
+        {
+            //loginDtoRecord.Email = string.Empty; //bir defa kullanılacak olduğu için başka bir değer atanamaz
+            return Ok("başarılı");
+        }
+
+    }
+}
---
+++ Record/README.md
@@ -0,0 +1,4 @@
+```
+Eðer uygulamada bir defa atayýp ikinci defa deðiþtirilmeyecek nesneler  varsa record'la tanýmlayýp ikinci defa oto. atanmasýnýn önüne geçilmiþ oluyor.
+DTO
+```
