b5364611b2598a752b6a5f4d3cd2e3f243383b41
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-17 16:24:49 +0300

    AsNoTracking

---
+++ AsNoTracking/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AsNoTracking;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BenchmarkRunner.Run<BencmarkService>();
+    }
+}
