273a101b74aa79ec2eb9986dfc78b81d04dc120b
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-17 14:47:27 +0300

    Add project files.

---
+++ DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context;
+
+//sealed başka bir class'a inherit edilmesini engelliyor
+public sealed class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=.;Database=TipsForNetDb;Trusted_Connection=True;MultipleActiveResultSets=true;Integrated Security=True");
+    }
+    DbSet<Product> Products { get; set; }
+}
---
+++ Entity/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Entity;
+
+public sealed class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
