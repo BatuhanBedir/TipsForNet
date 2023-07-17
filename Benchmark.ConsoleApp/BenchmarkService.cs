458dab32278202156624d9282b3761e79ebfa32f
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-17 16:00:05 +0300

    Benchmark

---
+++ Benchmark.ConsoleApp/BenchmarkService.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Benchmark.ConsoleApp;
+
+public class BenchmarkService
+{   //ShortRunJob -> kısa ve performanslı çalışmasını sağlıyor
+    [ShortRunJob,Config(typeof(Config))]
+    private class Config : ManualConfig
+    {
+        //değerlendirdiğim methodlarda veya yöntemlerde perf farkını yüzdelik olarak görmek için
+        public Config()
+        {
+            SummaryStyle = BenchmarkDotNet.Reports.SummaryStyle.Default.WithRatioStyle(BenchmarkDotNet.Columns.RatioStyle.Trend);
+        }
+    }
+    [Benchmark(Baseline = true)] //ana method olduğunu belirtme
+    public void GetAll()
+    {
+        AppDbContext context = new();
+        context.Products.ToList();
+    }
+    [Benchmark]
+    public void GetAllSqlRaw()
+    {
+        AppDbContext context = new();
+        context.Products.FromSqlRaw("Select * From Products").ToList();
+    }
+}
---
+++ Benchmark.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmark.ConsoleApp;
+
+internal class Program
+{
+    //Debug -> Release
+    //Debug tool-> Start Without Debugging
+
+    //uygulamalarda performans ölçmemizi sağlayan bir yöntem. Derlenmiş bir uygulama değil; çalışabilir, canlıya alınabilecek bir uygulamada denemek gerekiyor
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        BenchmarkRunner.Run<BenchmarkService>();
+    }
+}
