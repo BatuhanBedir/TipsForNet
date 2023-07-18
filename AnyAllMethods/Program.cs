49b940d4a42b066861d8edc7d04fd53dd8e62be9
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-18 13:12:00 +0300

    Any/All

---
+++ AnyAllMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Context;
+
+namespace AnyAllMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDbContext context = new();
+
+            bool result = context.Products.Any();
+
+            bool result2 = context.Products.Any(x=>x.Name == "Product1");
+
+            bool result3 = context.Products.All(x => x.Name.Contains("P"));
+
+            bool result4 = context.Products.All(x => x.Name == "Product1");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
