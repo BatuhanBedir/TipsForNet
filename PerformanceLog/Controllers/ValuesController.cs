7bb5753ebc6e38569390afa0b2b7f2b78271ab39
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-18 12:38:51 +0300

    PerformanceLog

---
+++ PerformanceLog/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Context;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PerformanceLogWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
+    {
+        AppDbContext context = new();
+
+        IList<PerformanceLog> performanceLogs = 
+            await context.PerformanceLogs
+            .OrderByDescending(x=>x.Id)
+            .ToListAsync();
+
+        return Ok(performanceLogs);
+    }
+
+    [HttpGet("[action]")]
+    public async Task<IActionResult> GetAllProductsAsync(CancellationToken cancellationToken)
+    {
+        AppDbContext context = new();
+        IList<Product> products = 
+            await context.Products
+            .ToListAsync(cancellationToken);
+        return Ok(products.Take(10));
+
+    }
+}
---
+++ PerformanceLog/README.md
@@ -0,0 +1,8 @@
+```
+Performans sorunlarýný giderirken yardýmcý olmak için tasarlanmýþtýr. Raporlar, bir madconfig hedefi tarafýndan oluþturulur
+
+ve uygulama sunucusu tarafýndan toplanan performans günlüklerine dayanýr.
+
+
+Raporun baþlatýldýðý ve durdurulduðu tarih ve saat ile raporlama sürecinin çalýþtýrýldýðý toplam süre kaydedilir
+```
